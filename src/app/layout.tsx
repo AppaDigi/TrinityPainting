fc6687cad870aacd58c51806344eabfe19b925d3
Kiefer Likens <extremekief@gmail.com>
2026-04-07 13:51:08 -0500

    style: Configured logo as site favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,12 @@ export const metadata: Metadata = {
       siteName: "Trinity Painting & Renewal",
       locale: "en_US",
       type: "website",
-  }
+  },
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
